4282f111ea3e37be52d099f01071c5394bc7aafa
scti1016 <scti1016@scti1016>
2017-11-05 23:54:12 +0100

    working angular

--- src/ini.js
+++ src/ini.js
@@ -1,15 +1,9 @@
-Character = require('./Characters/Character');
-CharacterList = require('./Characters/CharacterList');
 
 angular.module('app', [])
-    .controller('controller', function($scope) {
+    .controller('controller', function() {
         let list = this;
 
-        $scope.name = 'abc';
-
-        list.addCharacter(new Character('1',1));
-        list.addCharacter(new Character('2',2));
-        list.addCharacter(new Character('3',3));
+        this.name = 'abc';
 
         list.func = () => {
 
